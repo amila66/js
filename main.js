26d4369a7e0c03bc37e2720d172b580660fef6ea
My name <amila2002@example.com>
2023-08-01 20:54:46 +0530

    4

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
-person={
-    id:'001',
-    name:'nimal',
-    address:'colombo',
-    salary:'20000',
-};
-console.log(person);
+number1=0;
+number2=0;
+sum=()=>{
+     calculate("+")
+}
 
-console.log('============================');
-console.log(person.address);
+function calculate(exp) {
+    if (number1===0){
+        alert('Please insert a number!');
+        number1=document.
+        getElementById('textInput').value;
 
-console.log('============================');
-console.log(person['name']);
-
-function setData(brand, color, type){
-    //let var const
-    let vehicle= {
-        brand:brand,
-        color:color,
-        type:type,
-        calculate: function (){
-            return 25000/2;
-        }
     }
-    return vehicle;
-};
-tempVehicle=setData('BMW','black','Sport');
-console.log(tempVehicle);
-console.log(tempVehicle.calculate());
+    console.log(exp, number1);
+}
