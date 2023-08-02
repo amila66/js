494528fe388ca7b1f2e31b2727ad752a4df06f89
My name <amila2002@example.com>
2023-08-02 18:09:56 +0530

    8

--- main.js
+++ main.js
@@ -1,50 +1,57 @@
-let number1=0;
-let number2=0;
-sum=()=> calculate("+");
-mul=()=> calculate("*");
-div=()=> calculate("/");
-sub=()=> calculate("-");
-mod=()=> calculate("%");
+let number1 = 0;
+let number2 = 0;
 
-
+sum = () => calculate("+");
+mul = () => calculate("*");
+div = () => calculate("/");
+sub = () => calculate("-");
+mod = () => calculate("%");
 
 function calculate(exp) {
-    let tempvalue=Number(document.
-    getElementById('textInput').value);
+    let tempvalue = Number(document.getElementById('textInput').value);
 
-    if (isNaN(tempvalue) || tempvalue===0){
+    if (isNaN(tempvalue) || tempvalue === 0) {
         alert('please enter a valid number');
         return;
     }
-    if (number1===0){
-            number1=tempvalue;
-            document.getElementById('textInput').value=''
+
+    if (number1 === 0) {
+        number1 = tempvalue;
+        document.getElementById('textInput').value = '';
         return;
-    }else{
-        number2=tempvalue;
-        document.getElementById('textInput').value=''
+    } else {
+        number2 = tempvalue;
+        document.getElementById('textInput').value = '';
+
+        switch (exp) {
+            case '+':
+                document.getElementById('textInput').value = `${number1} ${exp} ${number2} = ${number1 + number2}`;
+                break;
+
+            case '-':
+                document.getElementById('textInput').value = `${number1} ${exp} ${number2} = ${number1 - number2}`;
+                break;
+
+
+            case '*':
+                document.getElementById('textInput').value = `${number1} ${exp} ${number2} = ${number1 * number2}`;
+                break;
+
+
+            case '/':
+                document.getElementById('textInput').value = `${number1} ${exp} ${number2} = ${number1 / number2}`;
+                break;
+
+
+            case '%':
+                document.getElementById('textInput').value = `${number1} ${exp} ${number2} = ${number1 % number2}`;
+                break;
+
+        }
     }
-
-    switch (exp){
-        case '+':
-            console.log(number1+number2);
-            break;
-
-        case '-':
-            console.log(number1-number2);
-            break;
-
-        case '*':
-            console.log(number1*number2);
-            break;
-
-
-        case '/':
-            console.log(number1/number2);
-            break
-
-        case '%':
-            console.log(number1%number2);
-            break;
-    }
+}
+refresh=()=>{
+    number1=0;
+    number2=0;
+    document.getElementById('textInput').value='';
 }
