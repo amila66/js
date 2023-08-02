d87551e535f2d05a03f6e62226bab1a3b08c0ed3
My name <amila2002@example.com>
2023-08-02 11:54:35 +0530

    7

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
 let number1=0;
 let number2=0;
-sum=()=>{
-    calculate("+");
-}
+sum=()=> calculate("+");
+mul=()=> calculate("*");
+div=()=> calculate("/");
+sub=()=> calculate("-");
+mod=()=> calculate("%");
+
+
 
 function calculate(exp) {
-    let tempvalue=document.
-    getElementById('textInput').value;
-    if (isNaN(tempvalue) || tempvalue===''){
+    let tempvalue=Number(document.
+    getElementById('textInput').value);
+
+    if (isNaN(tempvalue) || tempvalue===0){
         alert('please enter a valid number');
         return;
     }
     if (number1===0){
             number1=tempvalue;
+            document.getElementById('textInput').value=''
+        return;
+    }else{
+        number2=tempvalue;
+        document.getElementById('textInput').value=''
     }
-    console.log(exp, number1);
+
+    switch (exp){
+        case '+':
+            console.log(number1+number2);
+            break;
+
+        case '-':
+            console.log(number1-number2);
+            break;
+
+        case '*':
+            console.log(number1*number2);
+            break;
+
+
+        case '/':
+            console.log(number1/number2);
+            break
+
+        case '%':
+            console.log(number1%number2);
+            break;
+    }
 }
